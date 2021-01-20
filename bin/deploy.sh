7e4a64ff6583e608f6851b2f34950d75aea3a5ae
Jorge Hernandez <jorge.hernandez@acilia.es>
2021-01-20 20:58:28 +0000

    infra

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\n\e[94m DEPLOY! \e[0m\n"
+
+git checkout master
+git pull origin master
+
+cd infra/docker/prod
+docker-compose down
+docker-compose up -d --build
+cd ../../../
+
+cp infra/env/.env.prod .env.local
+
+docker exec toolbox-php composer dump-env prod
+docker exec toolbox-php composer install --no-dev --optimize-autoloader
+docker exec toolbox-php bin/console doctrine:schema:update --force
+docker run -v $(pwd):/home/app toolbox-node yarn install
+docker run -v $(pwd):/home/app toolbox-node yarn encore production
+
+docker exec toolbox-php bin/console cache:clear
+docker exec toolbox-php bin/console cache:warmup
